252b96cd8905e4fbc9071969c04f198f0f0df696
HimanshuBhanja <123287335+HimanshuBhanja@users.noreply.github.com>
2025-07-25 23:46:23 +0530

    Add files via upload

---
+++ Python_Task2.py
@@ -0,0 +1,18 @@
+"""
+Task 2: Create a Personalized Greeting
+
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+#1st task completed
+user_name = input("User's first name is :")
+user_surname = input("User's last name is :")
+
+#2nd task completed
+c = user_name + " " + user_surname
+print (c)
+
+#3rd task completed
+print("Hello",c,", it's a pleasure to connect with you.")
